ec71f90bda2025e9a9213b50cb4cc17bb5ad8506
JaviJarp <javi.jarp1@gmail.com>
2026-06-09 18:53:12 -0400

    Added WASD to the player controller

--- player.gd
+++ player.gd
@@ -37,7 +37,7 @@ func _physics_process(delta: float) -> void:
 			velocity.y = JUMP_VELOCITY
 
 	# --- 2. HANDLE HORIZONTAL MOVEMENT (X/Z Axis) ---
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	# Determine current speed based on mode
